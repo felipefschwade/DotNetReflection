3642f25739c0f8e992b38ab1c2bcda01499dea87
Felipe F. Schwade <felipefaeschwade@hotmail.com>
2018-01-30 19:51:33 -0300

    Aula 1 Atividade 5 - Criando a resposta com recursos utilizando o Assembly da aplicação.

--- Bytebank.Web/Infra/WebApplication.cs
+++ Bytebank.Web/Infra/WebApplication.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,23 +19,44 @@ namespace Bytebank.Web.Infra
 
         public void Iniciar()
         {
+            while (true) HandleRequisition();
+        }
+
+        private void HandleRequisition()
+        {
             var listener = new HttpListener();
             foreach (var prefixo in _prefixos) listener.Prefixes.Add(prefixo);
-
             listener.Start();
 
             var context = listener.GetContext();
             var response = context.Response;
             var request = context.Request;
 
-            var respText = "Hello World!";
-            var repoBytes = Encoding.UTF8.GetBytes(respText);
-            response.StatusCode = 200;
-            response.ContentType = "text/html";
-            response.ContentLength64 = repoBytes.Length;
-            response.OutputStream.Write(repoBytes, 0, repoBytes.Length);
-            response.OutputStream.Close();
+            var path = request.Url.AbsolutePath;
 
+            if (path == "/Assets/css/styles.css")
+            {
+                var assembly = Assembly.GetExecutingAssembly();
+                var resource = "Bytebank.Web.Assets.css.styles.css";
+                var resourceStream = assembly.GetManifestResourceStream(resource);
+                var resourceBytes = new Byte[resourceStream.Length];
+                resourceStream.Read(resourceBytes, 0, (int)resourceStream.Length);
+                response.StatusCode = 200;
+                response.ContentType = "text/css";
+                response.ContentLength64 = resourceBytes.Length;
+                response.OutputStream.Write(resourceBytes, 0, resourceBytes.Length);
+                response.OutputStream.Close();
+
+            } else
+            {
+                var respText = "Hello World!";
+                var repoBytes = Encoding.UTF8.GetBytes(respText);
+                response.StatusCode = 200;
+                response.ContentType = "text/html";
+                response.ContentLength64 = repoBytes.Length;
+                response.OutputStream.Write(repoBytes, 0, repoBytes.Length);
+                response.OutputStream.Close();
+            }
             listener.Stop();
         }
     }
